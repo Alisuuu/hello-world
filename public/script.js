b1e62d08323dfee6fb8f46352d163eedfa6f4d56
Alisuuu <132578134+Alisuuu@users.noreply.github.com>
2025-04-11 01:13:25 -0300

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,27 @@
+const socket = io()
+
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const messages = document.getElementById('messages')
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+  if (input.value) {
+    socket.emit('chat message', input.value)
+    input.value = ''
+  }
+})
+
+socket.on('chat message', function (msg) {
+  const item = document.createElement('li')
+  item.textContent = msg
+  messages.appendChild(item)
+  messages.scrollTop = messages.scrollHeight
+})
+
+// Carrega o computador Hyperbeam
+fetch('/computer')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('hb').src = data.embed_url
+  })
